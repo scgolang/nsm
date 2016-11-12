d453f3c560c0181a8596d6638f87930f802c7d7a
Brian Sorahan <bsorahan@leaf.ag>
2016-11-12 17:03:18 -0600

    update client example

--- client_test.go
+++ client_test.go
@@ -1,30 +1,33 @@
 package nsm
 
 import (
-	"testing"
+	"log"
 )
 
+// ExampleClient is a simple example of a non session manager client.
 type ExampleClient struct {
 	SessionInfo
 }
 
+// Open opens a session.
 func (c *ExampleClient) Open(info SessionInfo) (string, Error) {
 	c.SessionInfo = info
 	// Do what you need to do to open the session.
 	return "Client has finished opening the session", nil
 }
 
+// Save saves a session.
 func (c *ExampleClient) Save() (string, Error) {
 	// Do what you need to do to save the session.
 	return "Client has finished saving the session", nil
 }
 
-func Example_client(t *testing.T) {
+func Example_client() {
 	c, err := NewClient(ClientConfig{
 		Session: &ExampleClient{},
 	})
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
 	c.Wait()
 }
