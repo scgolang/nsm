7897a059546abe9d8a55ca61a5fd3a2c44533c6b
Brian Sorahan <bsorahan@leaf.ag>
2016-11-12 16:52:32 -0600

    split out documentation

--- client.go
+++ client.go
@@ -1,5 +1,3 @@
-// Package nsm implements the non session manager OSC protocol.
-// See http://non.tuxfamily.org/nsm/API.html for more details.
 package nsm
 
 import (
---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package nsm implements the non session manager OSC protocol.
+//
+// See http://non.tuxfamily.org/nsm/API.html for more details.
+//
+// https://godoc.org/github.com/scgolang/nsm#ex-Client
+package nsm
