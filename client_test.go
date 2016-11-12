cc6044843a9213fc545b7b0ba8089f05a8b1c518
Brian Sorahan <bsorahan@leaf.ag>
2016-11-12 16:49:58 -0600

    add code to client example

--- client_test.go
+++ client_test.go
@@ -4,6 +4,27 @@ import (
 	"testing"
 )
 
+type ExampleClient struct {
+	SessionInfo
+}
+
+func (c *ExampleClient) Open(info SessionInfo) (string, Error) {
+	c.SessionInfo = info
+	// Do what you need to do to open the session.
+	return "Client has finished opening the session", nil
+}
+
+func (c *ExampleClient) Save() (string, Error) {
+	// Do what you need to do to save the session.
+	return "Client has finished saving the session", nil
+}
+
 // ClientExample
 func ClientExample(t *testing.T) {
+	_, err := NewClient(ClientConfig{
+		Session: &ExampleClient{},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
 }
